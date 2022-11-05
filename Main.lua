9cfe3ca22602c77f2315f1af8e91baa553f676fe
c8KSDD4y#h$sl7ExP <96740743+Lyalekin@users.noreply.github.com>
2022-11-05 22:15:10 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,7 @@
+-- dont judge :rage:
+
+warn("Seakui - Executed!")
+
+if game.PlaceId == 4042427666 then -- Anime Fighting Simulator
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Lyalekin/Roblox/AnimeFightingSim.lua'))()
+end
