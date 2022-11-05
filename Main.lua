493b9ed559b32fb22f517b30bad66be44f23b828
c8KSDD4y#h$sl7ExP <96740743+Lyalekin@users.noreply.github.com>
2022-11-05 22:57:01 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,20 @@
 -- dont judge :rage:
 
 warn("Seakui - Executed!")
+game.StarterGui:SetCore("SendNotification",  {
+    Title = "Seakui";
+    Text = "Executed!";
+    Icon = "rbxassetid://11478672932";
+    Duration = "5";
+})
 
-if game.PlaceId == 4042427666 then -- Anime Fighting Simulator
+-- antiafk
+local bb=game:service'VirtualUser'
+game:service'Players'.LocalPlayer.Idled:connect(function()
+bb:CaptureController()bb:ClickButton2(Vector2.new())
+end)
+
+-- anime fighting simulator
+if game.PlaceId == 4042427666 then
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Lyalekin/Roblox/AnimeFightingSim.lua'))()
 end
